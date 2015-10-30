ff9b51ec0c77daa1cfac24f9778da090c9f12723
Jonathan Neal <jonathantneal@hotmail.com>
2015-10-29 21:27:19 -0400

    0.3.0

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+var Section = require('./lib/Section.js');
+
 module.exports = require('postcss').plugin('mdcss', function (opts) {
 	opts = opts || {};
 
-	opts.theme = opts.theme || require('mdcss-theme-github');
+	opts.theme     = opts.theme     || require('mdcss-theme-github');
+	opts.directory = opts.directory || 'styleguide';
+	opts.template  = opts.template  || 'template.ejs';
+	opts.index     = opts.index     || 'index.html';
 
 	return function (css, result) {
-		if (typeof opts.theme === 'function') opts.theme(css, opts, process.cwd());
+		var section = new Section(css);
+
+		if (typeof opts.theme === 'function') opts.theme(section, opts, result);
 		else result.warn('The theme failed to load.');
 	};
 });
